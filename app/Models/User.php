526005b911609fc9c2c9e99c1b5ba4cc917e7f0e
buguenocesar92 <buguenocesar92@gmail.com>
2025-04-27 17:31:46 -0400

    Refactor User model and ExampleTest: remove unused factory usage and update test method for clarity

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,6 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
